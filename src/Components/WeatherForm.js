722106cdecd2ea109cec159def17a8313d05c4c9
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-07 19:03:32 +0545

    done

--- src/Components/WeatherForm.js
+++ src/Components/WeatherForm.js
@@ -1,17 +1,20 @@
-// src/components/WeatherForm.js
+
 import React from "react";
 
 
 function WeatherForm({ city, onCityChange, onSearch }) {
   return (
-    <div className="weather-form">
+    <div className="weather-form animate__animated animate__fadeIn">
       <input
         type="text"
         value={city}
         onChange={onCityChange}
         placeholder="Enter city name"
+        className="animate__animated animate__pulse"
       />
-      <button onClick={onSearch}>Search</button>
+      <button onClick={onSearch} className="animate__animated animate__bounce">
+        Search
+      </button>
     </div>
   );
 }
