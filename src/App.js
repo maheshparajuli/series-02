339cb0132246b1c7c535ce92910db8edb73acde3
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-07 20:39:17 +0545

    some updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
 import React from "react";
-import './App.css';
-import './Weatherform.css'
+import './App.css'; 
+import './Weatherform.css';  
 import Weather from "./Components/Weather";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 className="app-title">Weather App</h1>
       <Weather />
     </div>
   );
