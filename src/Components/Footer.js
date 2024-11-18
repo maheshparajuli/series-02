8e3b064b7cc46a64b9331c3d3a26c874b0a46cd2
maheshparajuli <maheshparajuli717@gmail.com>
2024-11-18 23:30:39 +0545

    done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Mail, Github } from 'lucide-react';
+import { Mail } from 'lucide-react';
 
 function Footer() {
   return (
@@ -10,16 +10,6 @@ function Footer() {
             <Mail className="contact-icon" />
             <a href="mailto:contact@maheshparajuli.com">
               maheshparajuli717@gmail.com
-            </a>
-          </div>
-          <div className="contact-item">
-            <Github className="contact-icon" />
-            <a 
-              href="https://github.com/maheshparajuli" 
-              target="_blank" 
-              rel="noopener noreferrer"
-            >
-              GitHub Profile
             </a>
           </div>
         </div>
